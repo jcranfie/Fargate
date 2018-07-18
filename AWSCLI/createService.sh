b0fd94d588ab60e49ce3a3c74c24f1dd71de8337
Julian Cranfield <jcranfie@tibco.com>
2018-07-18 13:17:34 +0100

    Added autoscaling

--- AWSCLI/createService.sh
+++ AWSCLI/createService.sh
@@ -1,3 +1,7 @@
 
 aws ecs create-service --cluster testcluster --service-name AlertHubFargate-Service --task-definition AlertHub-fargate:5 --desired-count 2 --launch-type "FARGATE" --deployment-configuration "maximumPercent=200,minimumHealthyPercent=50"  --network-configuration "awsvpcConfiguration={subnets=[subnet-8ead62ea],securityGroups=[sg-5ee0f223],assignPublicIp=ENABLED}"
 
+aws application-autoscaling register-scalable-target --resource-id service/testcluster/AlertHubFargate-Service --service-namespace ecs --scalable-dimension ecs:service:DesiredCount --min-capacity 2 --max-capacity 20 --role-arn arn:aws:iam::675390051515:role/aws-service-role/ecs.application-autoscaling.amazonaws.com/AWSServiceRoleForApplicationAutoScaling_ECSService
+
+aws application-autoscaling put-scaling-policy --cli-input-json file://./AWSCLI/put-scaling-policy-customer.json
+
